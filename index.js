428b6320cc902584dd1e595b256daa52896a395e
Beth Wilson <bethwilson777@gmail.com>
2023-07-19 18:42:15 -0600

    Updated conversion button function for add listener

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-//query selector
-const convertBtn = document.getElementById("convert-btn");
-const inputEl = document.getElementById("input");
-const lengthEl = document.getElementById("length");
-const volumeEl = document.getElementById("volume");
-// const massEl = document.getElementById("mass");
-//event listener
-convertBtn.addEventListener("click", function() {
-    let input = inputEl.value;
-    let feet = input * 3.281;
-    let meters = input / 3.281;
-    let gallon = input * 0.264;
-    let liter = input / 0.264;
-
-
-    lengthEl.innerHTML += `
-        <p>${inputEl.value} meters = ${feet.toFixed(3)} feet | ${inputEl.value} feet = ${meters.toFixed(3)} meters</p>
-        `
-    volumeEl.innerHTML += ` 
-        <p>${inputEl.value} liters = ${gallon.toFixed(3)} gallons | ${inputEl.value} gallons = ${liter.toFixed(3)} liters</p>
-    `
-})
-
 /*
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
 
-//3 functions for unit conversion 
-//input value
-//conversion
-//.innerhtml
+//query selector
+const convertBtn = document.getElementById("convert-btn");
+const inputEl = document.getElementById("input");
+const lengthEl = document.getElementById("length");
+const volumeEl = document.getElementById("volume");
+const massEl = document.getElementById("mass");
+//event listener
+
+
+convertBtn.addEventListener("click", function() {
+    let input = inputEl.value;
+    const feet = input * 3.281;
+    const meters = input / 3.281;
+    const gallon = input * 0.264;
+    const liter = input / 0.264172;
+    const pound = input * 2.20462;
+    const kilogram = input / 2.20462;
+
+    lengthEl.innerHTML = `
+        <p>${inputEl.value} meters = ${feet.toFixed(3)} feet | ${inputEl.value} feet = ${meters.toFixed(3)} meters</p>
+        `
+    volumeEl.innerHTML = ` 
+        <p>${inputEl.value} liters = ${gallon.toFixed(3)} gallons | ${inputEl.value} gallons = ${liter.toFixed(3)} liters</p>
+    `
+    massEl.innerHTML = `
+        <p>${inputEl.value} kilos = ${pound.toFixed(3)} pounds | ${inputEl.value} pounds = ${kilogram.toFixed(3)} kilos</p>
+    `
+    inputEl.value = "";
+    
+})
+
